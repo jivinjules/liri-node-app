5242a8bcc055a9b4ba3ff4d047d38fce18613df4
jivinjules <juliehodges05@gmail.com>
2018-08-16 14:57:18 -0500

    added keys

--- liri.js
+++ liri.js
@@ -1,6 +1,20 @@
 require("dotenv").config();
 
-var keys2 = require("./keys.js");
-
+var keysFile = require("./keys.js");
+var bandsintown = require('bandsintown')(APP_ID);
 var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var request = require('request');
+
+
+//Commands for Liri to accomplish
+// * `concert-this`
+
+// * `spotify-this-song`
+
+// * `movie-this`
+
+// * `do-what-it-says`
+
+//Global variables
+//input for user to type a string word
+var inputString = process.argv;
